800635b6a8aec98641a10f055b0558be52b322dc
eCituk <ecituk@thefives.com.mx>
2020-10-09 17:51:41 -0500

    Organizando el Archivo Settings.py, se divide en otros archivos a parte, base, local y prod

---
+++ empleados/empleados/settings/local.py
@@ -0,0 +1,23 @@
+from .base import * # el punto de .base es para indicar que el archivo se encuentra en el mismo folder
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
