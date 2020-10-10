ca206e3aa407850ed9a72374e4e8a6c595915a56
eCituk <ecituk@thefives.com.mx>
2020-10-10 11:44:18 -0500

    Models para vistas en Django

--- empleados/applications/home/admin.py
+++ empleados/applications/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Prueba #importo el modelo recientemente creado, Prueba es una clase
 # Register your models here.
+
+admin.site.register(Prueba)
