684e3532ecfadd0fe7947640d54adf440181e174
eCituk <ecituk@thefives.com.mx>
2020-10-13 14:57:28 -0500

    conexion a postgress finalizada

--- empleados/empleados/settings/local.py
+++ empleados/empleados/settings/local.py
@@ -10,8 +10,8 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbempleado',
-        'USER': 'ecitukUser',
+        'NAME': 'empleado',
+        'USER': 'cituk',
         'PASSWORD': 'djangoapp',
         'HOST': 'localhost',
         'PORT': '5432',
