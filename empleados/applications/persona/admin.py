c155888cd8f8827556dda5a7189ba8744f9e526f
eCituk <ecituk@thefives.com.mx>
2020-10-14 10:58:15 -0500

    Personalizar Tablas en el Administrador Django

--- empleados/applications/persona/admin.py
+++ empleados/applications/persona/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Empleado
+from .models import Empleado, Habilidades
 # Register your models here.
-admin.site.register(Empleado)
+admin.site.register(Habilidades)
+
+#django en los models son trae el valor que _self indica, en este punto se modifica para agregar más campos mostrar 
+class EmpleadoAmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'departamento',
+        'job',
+    )
+admin.site.register(Empleado,EmpleadoAmin)
--- empleados/applications/persona/models.py
+++ empleados/applications/persona/models.py
@@ -2,9 +2,17 @@ from django.db import models
 #importacion del modelo con que estará relacionado nuestra tabla de empleados
 from applications.departamento.models import Departamento
 
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
 
-# Modelo para la tabla empleado.
-class Empleado(models.Model):
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades empleados'
+
+    def __str__(self):
+        return str( self.id ) + '-' + self.habilidad 
+
+class Empleado(models.Model): # Modelo para la tabla empleado.
 
     job_choices = (
         ('0','Contador'),
@@ -17,8 +25,16 @@ class Empleado(models.Model):
     first_name = models.CharField('Nombres', max_length=60) #Nombres, asi lo verá el admin de django
     last_name = models.CharField('Apellidos', max_length=60) #Apellidos, asi lo verá el admin de django
     job = models.CharField('Trabajo', max_length=50, choices=job_choices) 
-    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE) #columna de relacion con la tabla Departamento
-    #image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE) #columna de relacion con la tabla Departamento, relacion de 1 a muchos
+    #avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
+
+    class Meta:
+        verbose_name = 'Mi empleado'
+        verbose_name_plural = 'Empleados de la empresa'
+        ordering = ['first_name','last_name'] #se ordena por el campo name
+        #ordering = ['-name'] #se ordena por el campo name, de la z a la a
+        unique_together = ('first_name' , 'last_name') # valida que no se pueda generar registros con la combinacion entre 'first_name' y 'last_name'
 
     def __str__(self):
         return str( self.id ) + '-' + self.first_name + ' ' + self.last_name 
