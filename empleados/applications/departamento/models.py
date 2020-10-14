30f71d32c77e906ed04b216bd46000ffb25693d5
eCituk <ecituk@thefives.com.mx>
2020-10-14 10:19:37 -0500

    Class Meta, modelos y en el administrador Django

--- empleados/applications/departamento/models.py
+++ empleados/applications/departamento/models.py
@@ -7,5 +7,14 @@ class Departamento(models.Model):
     shor_name = models.CharField('Nombre corto', max_length=20, unique=True) #Nombre corto, asi lo verá el admin de django
     anulate = models.BooleanField('Anulado',default=False)#Anulado, asi lo verá el admin de django y por defecto falso
 
+    class Meta:
+        verbose_name = 'Mi departamento'
+        verbose_name_plural = 'Áreas de la empresa'
+        ordering = ['name'] #se ordena por el campo name
+        #ordering = ['-name'] #se ordena por el campo name, de la z a la a
+        #unique_together = ('name' , 'shor_name') # valida que no se pueda generar registros con la combinacion entre 'name' y 'shor_name'
+
+
+
     def __str__(self):
         return str( self.id ) + '-' + self.name + ' ' + self.shor_name 
