ef793ada6f3b39f4c77f200cc0d21ef8f3282f7c
eCituk <ecituk@thefives.com.mx>
2020-10-21 18:37:47 -0500

    envio de correo al hacer checkout

---
+++ entornos/pos/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ecituk\documents\python\entornos\pos\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ pos/djecommerce/settings/development.py
@@ -0,0 +1,54 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+# DEBUG TOOLBAR SETTINGS
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': 'C:\Users\Conrado\proyectos\fives\thefivesales\MXthefivesales',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
+
+# Email settings
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = '8ebffee91228d3'
+EMAIL_HOST_PASSWORD = '95dbf6bb50c42b'
+EMAIL_PORT = '2525'
